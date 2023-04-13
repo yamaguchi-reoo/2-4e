82b8a3b6d0719673599b6c636c39241349e8c066
れお <120348353+yamaguchi-reoo@users.noreply.github.com>
2023-04-13 16:55:13 +0900

    Update main.cpp

--- ゲーム制作/main.cpp
+++ ゲーム制作/main.cpp
@@ -25,7 +25,7 @@ int gGameState = 0;			//　ゲームモード
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR IpCmdLine, _In_ int nCmdShow)
 {
 	// タイトルを設定
-	SetMainWindowText("りんご");
+	SetMainWindowText("りんごごりら");
 
 	// ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
