84f23c8194620cda9175538a3a6914c92ebed658
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2023-04-20 15:02:16 +0900

    delete

--- GameJam/GameJam/Help.cpp
+++ GameJam/GameJam/Help.cpp
@@ -8,32 +8,7 @@
 // コンストラクタ
 //--------------------------------
 Help::Help() {
-	//image = LoadGraph("images/help.png");
-	//ヘルプ画面読み込み
-	if ((HelpImage = LoadGraph("Resource/Images/Help.png")) == -1)
-	{
-		throw "Resource/Images/Help.png";
-	}
-	//BGMの読み込み
-	if ((HelpBGM = LoadSoundMem("Resource/sounds/BGM/Sick_Red_Girl.wav")) == -1)
-	{
-		throw "Resource/sounds/BGM/Sick_Red_Girl.wav";
-	}
-	//BGMの音量変更
-	ChangeVolumeSoundMem(125, HelpBGM);
-
-	//SEの読み込み
-	if ((DecisionSE = LoadSoundMem("Resource/sounds/SE/Decision.wav")) == -1)
-	{
-		throw "Resource/sounds/SE/Decision.wav";
-	}
-	//SEの音量変更
-	ChangeVolumeSoundMem(125, DecisionSE);
-
-	//BGMの再生
-	if (CheckSoundMem(HelpBGM) == 0) {
-		PlaySoundMem(HelpBGM, DX_PLAYTYPE_BACK, TRUE);
-	}
+	
 }
 
 //--------------------------------
@@ -41,9 +16,7 @@ Help::Help() {
 //--------------------------------
 Help::~Help()
 {
-	//サウンドの削除
-	DeleteSoundMem(HelpBGM);
-	DeleteSoundMem(DecisionSE);
+
 }
 
 //--------------------------------
@@ -51,23 +24,7 @@ Help::~Help()
 //--------------------------------
 AbstractScene* Help::Update() 
 {
-	
 
-	//Bボタンでタイトルへ戻る
-	if (PAD_INPUT::OnButton(XINPUT_BUTTON_B))
-	{
-		StopSoundMem(HelpBGM);
-		PlaySoundMem(DecisionSE, DX_PLAYTYPE_BACK);
-		return new Title();
-	}
-	//Aボタンでゲームを始める
-	if (PAD_INPUT::OnButton(XINPUT_BUTTON_A)) 
-	{
-		StopSoundMem(HelpBGM);
-		PlaySoundMem(DecisionSE, DX_PLAYTYPE_BACK);
-		return new GameMain();
-	}
-	return this;
 }
 
 //--------------------------------
@@ -75,5 +32,5 @@ AbstractScene* Help::Update()
 //--------------------------------
 void Help::Draw()const 
 {
-	DrawGraph(0, 0, HelpImage, FALSE);
+	
 }
